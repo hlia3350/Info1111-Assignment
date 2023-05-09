cf7ac289193cdcc3ee4e453f393d8a0f566e6c21
Young <128681080+hlia3350@users.noreply.github.com>
2023-05-10 01:00:27 +1000

    Pause function
    This is the new function which added to the game

---
+++ pause.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class pause : MonoBehaviour
+{
+	bool isPaused = false;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			isPaused = !isPaused;
+			if (isPaused)
+			{
+				Time.timeScale = 0;
+			}
+			else
+			{
+				Time.timeScale = 1;
+			}
+		}
+	}
+
+	void OnGUI()
+	{
+		if (isPaused)
+		{
+			GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 25, 100, 50), "Paused");
+		}
+	}
+}
